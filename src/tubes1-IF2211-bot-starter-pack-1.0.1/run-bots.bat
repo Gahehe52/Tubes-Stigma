a6e675dd870bed7ea9f0e42e5b87f37b167608c9
Ghama <ghamaalfajri@gmail.com>
2025-06-01 23:11:59 +0700

    Add files via upload

---
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -0,0 +1,4 @@
+@echo off
+start cmd /c "python main.py --logic Density --email=cobac@email.com --name=density --password=123456 --team etimo"
+start cmd /c "python main.py --logic Profit --email=coba1@email.com --name=profit --password=123456 --team etimo"
+start cmd /c "python main.py --logic Weight --email=coba2@email.com --name=weight --password=123456 --team etimo"
